15f4b9072acf8bd18d2844d2baa117bc389fc338
Barayuda Gautama <wb-igedebara614600@antfin.com>
2020-04-25 22:34:48 +0800

    update role model and adding role seeder

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = ['admin', 'private', 'public', 'default'];
+        for ($i=0; $i <= count($roles); $i++) {
+            foreach ($roles as $role) {
+                $role = new Role;
+                $role->role_name = $roles[$i];
+                $role->save();
+                $i+=1;
+            }
+        }
+        $role->save();
+    }
+}
