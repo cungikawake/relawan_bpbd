3e630bad86564ea1e539ec5cc822df05ff3a8c3c
putu nikayana <cungikawake@gmail.com>
2020-11-06 23:21:00 +0800

    fix

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -19,8 +19,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
               <div class="slider-content">
                 <!-- layer 1 -->
-                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s">
-                  <h2 class="title1">Relawan Penanggulangan Bencana</h2>
+                <div class="layer-1-1 hidden-xs wow slideInDown" data-wow-duration="2s" data-wow-delay=".2s"> 
                 </div>
                 <!-- layer 2 -->
                 <div class="layer-1-2 wow slideInUp" data-wow-duration="2s" data-wow-delay=".1s">
